3f06d219a45a8aae394296def30bf54ec4d50ea4
Aditi Tiwari <115400068+Aditiwari7@users.noreply.github.com>
2023-06-09 20:01:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function createTable() {
     //Write your code here
-  
+	var rn = parseInt(prompt("Input number of rows"));
+	var cn = parseInt(prompt("Input number of columns"));
+
+	var table = document.getElementById("myTable");
+
+	table.innerHTML = "";
+
+	for (let i = 0; i < rn; i++) {
+		var newRow = table.insertRow(i);
+		for (let i = 0; i < cn; i++) {
+			var newCell = newCol.insertCell(j);
+			newCell.innerHTML = "Row-"+i+"Column-"+j:
+		}
+	}
 }
